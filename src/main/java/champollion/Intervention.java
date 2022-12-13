9a89989e5f32ba2dac9a73aa3bdb2c5248b2a89d
Damien Lopez <damien-lopez@etud.univ-jfc.fr>
2022-12-13 12:50:03 +0100

    commit final 2

--- src/main/java/champollion/Intervention.java
+++ src/main/java/champollion/Intervention.java
@@ -1,40 +1,42 @@
 package champollion;
 
-import java.time.*;
+import java.util.*;
 
 public class Intervention {
-    public LocalDate debut;
+    public Date debut;
     public int duree;
     public boolean annulee;
     private int heuredebut;
     private TypeIntervention type;
     private Salle lieu;
+    private UE matiere;
 
-    public Intervention(LocalDate debut, int duree, int heuredebut, TypeIntervention type) {
+    public Intervention(Date debut, int duree, int heuredebut, UE matiere, TypeIntervention type) {
         this.debut = debut;
         this.duree = duree;
         this.annulee = false;
         this.heuredebut = heuredebut;
         this.type = type;
+        this.matiere = matiere;
 
     }
 
     public boolean reservation(Salle salle) {
         boolean flag = true;
         for (int i = 0; i < salle.Interventionprevues.size(); i++) {
-            if (this.debut == salle.Interventionprevues.get(i).getDate()) {
-                if (this.heuredebut == salle.Interventionprevues.get(i).getHeureDebut() ||
-                        (this.heuredebut < salle.Interventionprevues.get(i).getHeureDebut()
+            if (this.debut == salle.Interventionprevues.get(i).debut) {
+                if (this.heuredebut == salle.Interventionprevues.get(i).heuredebut ||
+                        (this.heuredebut < salle.Interventionprevues.get(i).heuredebut
                                 + salle.Interventionprevues.get(i).getDuree() &&
-                                this.heuredebut > salle.Interventionprevues.get(i).getHeureDebut()
+                                this.heuredebut > salle.Interventionprevues.get(i).heuredebut
                                         + salle.Interventionprevues.get(i).getDuree())
                         ||
-                        (this.heuredebut + this.duree < salle.Interventionprevues.get(i).getHeureDebut()
+                        (this.heuredebut + this.duree < salle.Interventionprevues.get(i).heuredebut
                                 + salle.Interventionprevues.get(i).getDuree() &&
-                                this.heuredebut + this.duree > salle.Interventionprevues.get(i).getHeureDebut())
+                                this.heuredebut + this.duree > salle.Interventionprevues.get(i).heuredebut)
                         ||
-                        this.heuredebut + this.duree == salle.Interventionprevues.get(i).getHeureDebut()) {
-                    if (salle.Interventionprevues.get(i).getAnulee() == false) {
+                        this.heuredebut + this.duree == salle.Interventionprevues.get(i).heuredebut) {
+                    if (salle.Interventionprevues.get(i).annulee == false) {
                         flag = false;
                     }
                 }
@@ -46,48 +48,20 @@ public class Intervention {
         return flag;
     }
 
-    public Salle getLieu() {
-        return this.lieu;
+    public UE getMatiere() {
+        return this.matiere;
     }
 
-    public LocalDate getDate() {
-        return this.debut;
-    }
-
-    public void setDate(LocalDate newDate) {
-        this.debut = newDate;
+    public TypeIntervention getType() {
+        return this.type;
     }
 
     public int getDuree() {
         return this.duree;
     }
 
-    public void setDuree(int newDuree) {
-        this.duree = newDuree;
-    }
-
-    public boolean getAnulee() {
-        return this.annulee;
-    }
-
-    public void setAnulee(boolean annule) {
-        this.annulee = annule;
-    }
-
-    public int getHeureDebut() {
-        return this.heuredebut;
-    }
-
-    public void setHeureDebut(int newHeure) {
-        this.heuredebut = newHeure;
-    }
-
-    public TypeIntervention getTypeIntervention() {
-        return this.type;
-    }
-
-    public void setTypeIntervention(TypeIntervention newType) {
-        this.type = newType;
+    public void setAnnulee(boolean annulation) {
+        this.annulee = annulation;
     }
 
 }
