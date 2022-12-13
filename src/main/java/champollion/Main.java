6ce896646ddae9b566faa899943995a024502bfb
Damien Lopez <damien-lopez@etud.univ-jfc.fr>
2022-12-13 12:49:53 +0100

    commit final

--- src/main/java/champollion/Main.java
+++ src/main/java/champollion/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Enseignant bastide = new Enseignant("Rémi Bastide", "Remi.Bastide@irit.fr");
         Enseignant lamine  = new Enseignant("Elyes Lamine", "Elyes.Lamine@univ-jfc.fr");
         
-        UE uml = new UE("Conception par objets avec UML");
-        UE bd  = new UE("Bases de données");
-        UE web = new UE("Technologies web");
+        UE uml = new UE("Conception par objets avec UML", 80, 25, 20);
+        UE bd  = new UE("Bases de données", 20, 40, 20);
+        UE web = new UE("Technologies web", 30, 20, 50);
         
         bastide.ajouteEnseignement(uml, 12, 20, 20);
         bastide.ajouteEnseignement(web,  8, 20, 30);
@@ -15,12 +15,15 @@ public class Main {
         lamine.ajouteEnseignement(web,  15, 15, 25);
 
         System.out.printf("Mr. %s a un total de %d heures prévues\n", bastide.getNom(), bastide.heuresPrevues());
+
         System.out.printf("Mr. %s a un total de %d heures prévues\n",  lamine.getNom(),  lamine.heuresPrevues());
+
         System.out.printf("Mr. %s a un total de %d heures prévues dans l'UE %s\n", 
                 bastide.getNom(), 
                 bastide.heuresPrevuesPourUE(uml),
                 uml.getIntitule()
         );
+        
         
         
     }
